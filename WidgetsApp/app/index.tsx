50233066e5b61b89e544b7ccee456ad1cd0961d3
Irtaza <irtazanaqvi05@gmail.com>
2025-01-06 14:02:04 +0500

    added vercel.json and web build

--- WidgetsApp/app/index.tsx
+++ WidgetsApp/app/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
-import { NavigationContainer } from "@react-navigation/native";
 
 import HomeScreen from "./screens/HomeScreen";
 import AuthorSelectionScreen from "./screens/AuthorSelectionScreen";
@@ -24,18 +23,13 @@ export default function App() {
     return <AppLoading />;
   }
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen
-          name="Author Selection"
-          component={AuthorSelectionScreen}
-        />
-        <Stack.Screen name="Poet Selection" component={PoetSelectionScreen} />
-        <Stack.Screen name="Verse Details" component={VerseDetailsScreen} />
-        <Stack.Screen name="Settings" component={SettingsScreen} />
-        <Stack.Screen name="Share" component={ShareScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Author Selection" component={AuthorSelectionScreen} />
+      <Stack.Screen name="Poet Selection" component={PoetSelectionScreen} />
+      <Stack.Screen name="Verse Details" component={VerseDetailsScreen} />
+      <Stack.Screen name="Settings" component={SettingsScreen} />
+      <Stack.Screen name="Share" component={ShareScreen} />
+    </Stack.Navigator>
   );
 }
